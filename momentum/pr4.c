99193a3326f2c2af60d8ea66261a1278216aa2c4
prachi_cr7 <149580593+prachis70@users.noreply.github.com>
2023-11-24 18:37:39 +0530

    Add files via upload

---
+++ momentum/pr4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>.
+main()
+{
+  char ch;  
+   printf("Input day : "); 
+   scanf("%c",&ch);
+   switch(ch)  
+   {
+	case'm': printf("monday"); 
+	       break;
+	case 'n': printf("tuesday");
+	       break;
+	case 'o': printf("wednesday"); 
+	       break;
+	case 'p': printf("thursday");
+	        break;
+	case 'q': printf("friday");
+	       break;
+	case 'r': printf("saturday"); 
+	       break;
+	case 's': printf("saturday"); 
+	       break;
+      
+
+    }
+}
